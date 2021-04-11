fa65715568aeaf5115c7284130364ec5608afa8d
xiakai007 <980385778@qq.com>
2021-04-11 16:58:34 +0800

    xiakai 20210411-1 first commit

---
+++ common/src/main/java/com/woniu/mall/common/api/IErrorCode.java
@@ -0,0 +1,9 @@
+package com.woniu.mall.common.api;
+
+/**
+ * 封装API的错误码
+ */
+public interface IErrorCode {
+    long getCode();
+    String getMessage();
+}
---
+++ common/src/main/java/com/woniu/mall/common/domain/UserDto.java
@@ -0,0 +1,19 @@
+package com.woniu.mall.common.domain;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+@NoArgsConstructor
+public class UserDto {
+    private Long id;
+    private String username;
+    private String password;
+    private Integer status;
+    private String clientId;
+    private List<String> roles;
+}
